f63972f9764e7604d1c5f8c3d2a73b839de2e9af
scottbedard <scottbedard90@gmail.com>
2016-10-19 16:51:06 -0700

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,98 @@
+<?php namespace Bedard\Shop;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Shop Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Shop',
+            'description' => 'No description provided yet...',
+            'author'      => 'Bedard',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Bedard\Shop\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'bedard.shop.some_permission' => [
+                'tab' => 'Shop',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'shop' => [
+                'label'       => 'Shop',
+                'url'         => Backend::url('bedard/shop/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['bedard.shop.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+
+}
