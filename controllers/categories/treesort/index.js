9b65677367f5ea4ef11b7927a08e2a31f9c647cc
scottbedard <scottbedard90@gmail.com>
2016-10-21 15:09:49 -0700

    Enable JSX transforms

--- controllers/categories/treesort/index.js
+++ controllers/categories/treesort/index.js
@@ -4,6 +4,7 @@ import TreesortComponent from './treesort';
 $(() => {
     new Vue({
         el: '[data-bedard-shop="categories-treesort"]',
-        render: (h) => h(TreesortComponent),
+        functional: true,
+        render: h => h(TreesortComponent),
     });
 });
