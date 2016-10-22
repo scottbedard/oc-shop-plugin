316831a1ce215b37f71d1a5bc54e0df9990a2a8b
Scott Bedard <scottbedard90@gmail.com>
2016-10-22 18:24:38 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- updates/dev_seeds.php
+++ updates/dev_seeds.php
@@ -9,22 +9,24 @@ class DevSeeder extends Seeder
     public function run()
     {
         // only run the seeder in development
-        if (app()->env !== 'dev') return;
+        if (app()->env !== 'dev') {
+            return;
+        }
 
         $this->seedCategories();
     }
 
     protected function seedCategories()
     {
-        $clothes = Factory::create(new Category, [ 'name' => 'Clothes', 'slug' => 'clothes' ]);
-        Factory::create(new Category, [ 'name' => 'Shirts', 'slug' => 'shirts', 'parent_id' => $clothes->id ]);
-        Factory::create(new Category, [ 'name' => 'Pants', 'slug' => 'pants', 'parent_id' => $clothes->id ]);
-        Factory::create(new Category, [ 'name' => 'Shoes', 'slug' => 'shoes', 'parent_id' => $clothes->id ]);
-        $electronics = Factory::create(new Category, [ 'name' => 'Electronics', 'slug' => 'electronics' ]);
-        Factory::create(new Category, [ 'name' => 'Phones', 'slug' => 'phones', 'parent_id' => $electronics->id ]);
-        $computers = Factory::create(new Category, [ 'name' => 'Computers', 'slug' => 'computers', 'parent_id' => $electronics->id ]);
-        Factory::create(new Category, [ 'name' => 'Mac', 'slug' => 'mac', 'parent_id' => $computers->id ]);
-        Factory::create(new Category, [ 'name' => 'PC', 'slug' => 'pc', 'parent_id' => $computers->id ]);
-        Factory::create(new Category, [ 'name' => 'Televisions', 'slug' => 'televisions', 'parent_id' => $electronics->id ]);
+        $clothes = Factory::create(new Category, ['name' => 'Clothes', 'slug' => 'clothes']);
+        Factory::create(new Category, ['name' => 'Shirts', 'slug' => 'shirts', 'parent_id' => $clothes->id]);
+        Factory::create(new Category, ['name' => 'Pants', 'slug' => 'pants', 'parent_id' => $clothes->id]);
+        Factory::create(new Category, ['name' => 'Shoes', 'slug' => 'shoes', 'parent_id' => $clothes->id]);
+        $electronics = Factory::create(new Category, ['name' => 'Electronics', 'slug' => 'electronics']);
+        Factory::create(new Category, ['name' => 'Phones', 'slug' => 'phones', 'parent_id' => $electronics->id]);
+        $computers = Factory::create(new Category, ['name' => 'Computers', 'slug' => 'computers', 'parent_id' => $electronics->id]);
+        Factory::create(new Category, ['name' => 'Mac', 'slug' => 'mac', 'parent_id' => $computers->id]);
+        Factory::create(new Category, ['name' => 'PC', 'slug' => 'pc', 'parent_id' => $computers->id]);
+        Factory::create(new Category, ['name' => 'Televisions', 'slug' => 'televisions', 'parent_id' => $electronics->id]);
     }
 }
