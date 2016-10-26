612aa77c2a07b322409524714e7bb4b3b6a1831f
scottbedard <scottbedard90@gmail.com>
2016-10-25 22:04:55 -0700

    Adding a few product seeds

--- updates/dev_seeds.php
+++ updates/dev_seeds.php
@@ -1,6 +1,7 @@
 <?php namespace Bedard\Shop\Updates;
 
 use Bedard\Shop\Models\Category;
+use Bedard\Shop\Models\Product;
 use Bedard\Shop\Tests\Factory;
 use October\Rain\Database\Updates\Seeder;
 
@@ -14,6 +15,7 @@ class DevSeeder extends Seeder
         }
 
         $this->seedCategories();
+        $this->seedProducts(10);
     }
 
     protected function seedCategories()
@@ -22,5 +24,15 @@ class DevSeeder extends Seeder
         $parent2 = Factory::create(new Category, ['name' => 'Parent 2', 'slug' => 'parent-2']);
         $child1 = Factory::create(new Category, ['name' => 'Child 1', 'slug' => 'child-1', 'parent_id' => $parent1->id]);
         $child2 = Factory::create(new Category, ['name' => 'Child 2', 'slug' => 'child-2', 'parent_id' => $parent2->id]);
+    }
+
+    protected function seedProducts($count)
+    {
+        $categories = Category::count();
+
+        for ($i = 0; $i < $count; $i++) {
+            $product = Factory::create(new Product, ['name' => 'Product ' . $i, 'slug' => 'product-' . $i]);
+            $product->categories()->sync([rand(1, $categories)]);
+        }
     }
 }
