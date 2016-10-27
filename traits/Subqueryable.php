29f74a3944de444dbca6f0877365c0df8c837572
scottbedard <scottbedard90@gmail.com>
2016-10-26 20:09:05 -0700

    Minor code cleaning

--- traits/Subqueryable.php
+++ traits/Subqueryable.php
@@ -6,6 +6,13 @@ use October\Rain\Database\Builder;
 trait Subqueryable
 {
     /**
+     * Get the table associated with the model.
+     *
+     * @return string
+     */
+    abstract public function getTable();
+
+    /**
      * Select from a subquery.
      *
      * @param  \October\Rain\Database\Builder
@@ -21,31 +28,4 @@ trait Subqueryable
 
         return $query->selectSub($q, $as);
     }
-
-    /*
-     * Join the query with a subquery. Warning, in order to use this method
-     * properly, the join must be executed at the start of the query. If
-     * it's added t the end of the query, the bindings won't match up.
-     *
-     * @param  \October\Rain\Database\Builder   $query      Query being joined to
-     * @param  \October\Rain\Database\Builder   $subquery   Subquery being joined
-     * @param  string                           $alias      Joined table alias
-     * @param  string                           $left       Left side of condition
-     * @param  string                           $operator   Join condition operator
-     * @param  string                           $right      Right side of condition
-     * @param  string                           $join       Join type [ join, leftJoin ]
-     * @return \October\Rain\Database\Builder
-     */
-    // public function scopeJoinSubquery($query, $subquery, $alias, $left, $operator, $right, $join = 'join')
-    // {
-    //     $self = $this->getTable().'.*';
-    //     if (! in_array($self, $query->getQuery()->columns)) {
-    //         $query->addSelect($self);
-    //     }
-
-    //     $subquery = $subquery->getQuery();
-    //     $raw = DB::raw('('.$subquery->toSql().') '.$alias);
-
-    //     return $query->$join($raw, $left, $operator, $right)->mergeBindings($subquery);
-    // }
 }
