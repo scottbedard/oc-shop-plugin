6f5cf36e941ff3b84cf0369176af6606f0be9d6c
Scott Bedard <scottbedard90@gmail.com>
2016-11-11 08:17:35 -0700

    Add a test for hasOptionValueIds scope

---
+++ tests/unit/models/InventoryTest.php
@@ -0,0 +1,27 @@
+<?php namespace Bedard\Shop\Tests\Unit\Models;
+
+use Bedard\Shop\Models\Inventory;
+use Bedard\Shop\Models\Option;
+use Bedard\Shop\Models\OptionValue;
+use Bedard\Shop\Models\Product;
+use Bedard\Shop\Tests\Factory;
+use Bedard\Shop\Tests\PluginTestCase;
+use Carbon\Carbon;
+
+class InventoryTest extends PluginTestCase
+{
+    protected $refreshPlugins = ['Bedard.Shop'];
+
+    public function test_hasOptionValueIds_scope()
+    {
+        $product = Factory::create(new Product);
+        $option = Factory::create(new Option, ['product_id' => $product->id]);
+        $optionValue = Factory::create(new OptionValue, ['option_id' => $option->id]);
+        $inventory1 = Factory::create(new Inventory);
+        $inventory2 = Factory::create(new Inventory);
+        $inventory2->optionValues()->sync([$optionValue->id]);
+
+        $this->assertEquals(1, Inventory::hasOptionValueIds($optionValue->id)->count());
+        $this->assertEquals($inventory2->id, Inventory::hasOptionValueIds($optionValue->id)->first()->id);
+    }
+}
