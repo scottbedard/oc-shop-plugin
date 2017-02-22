7ae7d94434724e052576d71aba427d061b7a8cf2
Scott Bedard <scottbedard90@gmail.com>
2017-02-22 05:48:57 -0700

    Enable hot-reloading

--- build/dev-client.js
+++ build/dev-client.js
@@ -3,7 +3,7 @@
 require('eventsource-polyfill');
 const hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true');
 
-hotClient.subscribe(function (event) {
+hotClient.subscribe(function(event) {
     if (event.action === 'reload') {
         window.location.reload();
     }
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -6,9 +6,9 @@ const webpackConfig = require('./webpack.base.conf');
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
 
 // @todo: add hot-reload related code to entry chunks
-// Object.keys(webpackConfig.entry).forEach(function (name) {
-//     webpackConfig.entry[name] = ['./build/dev-client'].concat(webpackConfig.entry[name]);
-// });
+Object.keys(webpackConfig.entry).forEach(function (name) {
+    webpackConfig.entry[name] = ['./build/dev-client'].concat(webpackConfig.entry[name]);
+});
 
 module.exports = merge(webpackConfig, {
     devtool: '#cheap-module-eval-source-map',
@@ -17,7 +17,7 @@ module.exports = merge(webpackConfig, {
         new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"development"' }}),
 
         // @todo: turn on hot reloading
-        // new webpack.HotModuleReplacementPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
 
         // disable default errors and replace them with friendly errors
         new webpack.NoEmitOnErrorsPlugin(),
