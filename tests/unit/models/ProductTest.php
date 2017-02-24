7e0d01af25682086e0b58f4f26993331fa1eb77e
Scott Bedard <scottbedard90@gmail.com>
2017-02-24 05:50:22 -0700

    Test adding an option to a product

--- tests/unit/models/ProductTest.php
+++ tests/unit/models/ProductTest.php
@@ -3,6 +3,7 @@
 use PluginTestCase;
 use Bedard\Shop\Models\Product;
 use Bedard\Shop\Classes\Factory;
+use Bedard\Shop\Models\Option;
 use Bedard\Shop\Models\Category;
 
 class ProductTest extends PluginTestCase
@@ -30,5 +31,24 @@ class ProductTest extends PluginTestCase
         $products = Product::selectStatus()->get();
         $this->assertEquals(0, $products->find($disabled->id)->status);
         $this->assertEquals(1, $products->find($enabled->id)->status);
+    }
+
+    public function test_saving_options()
+    {
+        $option = Factory::create(new Option);
+        $option->load('values');
+        $optionData = $option->toArray();
+        $optionData['value_data'] = $optionData['values'];
+        unset($optionData['values']);
+
+        $product = Factory::create(new Product, [
+            'options_inventories' => json_encode([
+                'inventories' => [],
+                'options' => [$optionData],
+            ]),
+        ]);
+
+        $this->assertEquals(1, $product->options()->count());
+        $this->assertEquals($option->id, $product->options()->first()->id);
     }
 }
