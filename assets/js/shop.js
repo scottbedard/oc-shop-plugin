ab3265e081407761576a53a18e84614e7582854f
scottbedard <scottbedard90@gmail.com>
2017-02-28 22:26:45 -0700

    Minor tweaks

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -3,4 +3,6 @@ require('../scss/global');
 
 // create a global event bus so October can communicate with our components
 import Vue from 'vue';
+Vue.config.productionTip = false;
+
 window.BedardShop = new Vue();
