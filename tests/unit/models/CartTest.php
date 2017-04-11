316abb8fa6a478053efa41bb3d54b1bd2ab18f61
scottbedard <scottbedard90@gmail.com>
2017-04-10 23:15:41 -0700

    Fix failing test

--- tests/unit/models/CartTest.php
+++ tests/unit/models/CartTest.php
@@ -1,6 +1,9 @@
 <?php namespace Bedard\Shop\Tests\Unit\Models;
 
+use Bedard\Shop\Classes\Factory;
 use Bedard\Shop\Models\Cart;
+use Bedard\Shop\Models\Inventory;
+use Bedard\Shop\Models\Product;
 use PluginTestCase;
 
 class CartTest extends PluginTestCase
