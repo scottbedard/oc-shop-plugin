e54b018a99ee86e1571de854b482bc4848f9b24b
Scott Bedard <scottbedard90@gmail.com>
2017-04-15 12:52:38 -0700

    Beginning to make better test seeds

--- updates/dev_seeds.php
+++ updates/dev_seeds.php
@@ -20,70 +20,164 @@ class Seeds extends Seeder
         echo '  Seeding test data...';
         echo "\n";
 
-        $this->seedCategories(5);
-        $this->seedProducts(5);
+        $this->seedCategories();
+        $this->seedProducts();
         $this->seedOptions();
         $this->seedInventories();
+        $this->attachProductsToCategories();
 
         echo "\n  Done.\n";
         echo "\n";
     }
 
-    protected function seedCategories($quantity)
+    protected function attachProductsToCategories()
     {
-        for ($i = 0; $i < $quantity; $i++) {
-            Factory::create(new Category);
-        }
+        Product::whereSlug('no-inventories')
+            ->firstOrFail()
+            ->categories()
+            ->attach(Category::whereSlug('parent')->firstOrFail());
+
+        Product::whereSlug('free-thing')
+            ->firstOrFail()
+            ->categories()
+            ->attach(Category::whereSlug('child-a')->firstOrFail());
+
+        Product::whereSlug('out-of-stock')
+            ->firstOrFail()
+            ->categories()
+            ->attach(Category::whereSlug('child-b')->firstOrFail());
+
+        Product::whereSlug('shirt')
+            ->firstOrFail()
+            ->categories()
+            ->attach(Category::whereSlug('grandchild')->firstOrFail());
+
+        Product::syncAllCategories();
+
+        echo "  - Attaching products to categories\n";
+    }
+
+    protected function seedCategories()
+    {
+        $parent = Factory::create(new Category, [
+            'name' => 'Parent',
+            'slug' => 'parent',
+        ]);
+
+        $childA = Factory::create(new Category, [
+            'name' => 'Child A',
+            'slug' => 'child-a',
+        ]);
+
+        $childB = Factory::create(new Category, [
+            'name' => 'Child B',
+            'slug' => 'child-b',
+        ]);
+
+        $grandchild = Factory::create(new Category, [
+            'name' => 'Grandchild',
+            'slug' => 'grandchild',
+        ]);
+
+        $childA->makeChildOf($parent);
+        $childB->makeChildOf($parent);
+        $grandchild->makeChildOf($childA);
 
         echo "  - Categories\n";
     }
 
     protected function seedInventories()
     {
-        Product::with('options.values')->get()->each(function ($product) {
-            // create a default inventory
-            Factory::create(new Inventory, ['product_id' => $product->id, 'quantity' => rand(0, 5)]);
+        // out of stock
+        Factory::create(new Inventory, [
+            'product_id' => Product::whereSlug('out-of-stock')->firstOrFail()->id,
+            'quantity' => 0,
+        ]);
 
-            // create an inventory for each of our options
-            $product->options->each(function ($option) use ($product) {
-                $option->values->each(function ($value) use ($product) {
-                    Factory::create(new Inventory, [
-                        'product_id' => $product->id,
-                        'quantity' => rand(0, 5),
-                    ])->values()->attach($value);
-                });
-            });
-        });
+        // free
+        Factory::create(new Inventory, [
+            'product_id' => Product::whereSlug('free-thing')->firstOrFail()->id,
+            'quantity' => 1,
+        ]);
+
+        // shirt
+        Factory::create(new Inventory, [
+            'product_id' => Product::whereSlug('shirt')->firstOrFail()->id,
+            'quantity' => 5,
+            'value_ids' => [1, 4],
+        ]);
+
+        Factory::create(new Inventory, [
+            'product_id' => Product::whereSlug('shirt')->firstOrFail()->id,
+            'quantity' => 5,
+            'value_ids' => [1, 5],
+        ]);
+
+        Factory::create(new Inventory, [
+            'product_id' => Product::whereSlug('shirt')->firstOrFail()->id,
+            'quantity' => 0,
+            'value_ids' => [1, 6],
+        ]);
 
         echo "  - Inventories\n";
     }
 
     protected function seedOptions()
     {
-        Product::all()->each(function ($product) {
-            Factory::create(new Option, [
-                'product_id' => $product->id,
-                'name' => 'Size',
-                'placeholder' => '-- select size --',
-                'value_data' => [
-                    ['id' => null, 'name' => 'Small', 'sort_order' => 0],
-                    ['id' => null, 'name' => 'Medium', 'sort_order' => 1],
-                    ['id' => null, 'name' => 'Large', 'sort_order' => 2],
-                ],
-            ]);
-        });
+        $shirt = Product::whereSlug('shirt')->firstOrFail();
+
+        $size = Factory::create(new Option, [
+            'name' => 'Size',
+            'placeholder' => '-- select size --',
+            'product_id' => $shirt->id,
+            'value_data' => [
+                ['id' => null, 'name' => 'Small', 'sort_order' => 0],
+                ['id' => null, 'name' => 'Medium', 'sort_order' => 1],
+                ['id' => null, 'name' => 'Large', 'sort_order' => 2],
+            ],
+        ]);
+
+        $size = Factory::create(new Option, [
+            'name' => 'Color',
+            'placeholder' => '-- select color --',
+            'product_id' => $shirt->id,
+            'value_data' => [
+                ['id' => null, 'name' => 'Red', 'sort_order' => 0],
+                ['id' => null, 'name' => 'Green', 'sort_order' => 1],
+                ['id' => null, 'name' => 'Blue', 'sort_order' => 2],
+            ],
+        ]);
 
         echo "  - Options\n";
     }
 
-    protected function seedProducts($quantity)
+    protected function seedProducts()
     {
-        $categories = Category::all();
+        $noInventories = Factory::create(new Product, [
+            'description_html' => '<p>This product is out of stock because it had no inventories</p>',
+            'name' => 'No inventories',
+            'slug' => 'no-inventories',
+        ]);
 
-        for ($i = 0; $i < $quantity; $i++) {
-            $product = Factory::create(new Product);
-            $product->categories()->attach($categories->random(1));
-        }
+        $outOfStock = Factory::create(new Product, [
+            'description_html' => '<p>This product is out of stock</p>',
+            'name' => 'Out of stock',
+            'slug' => 'out-of-stock',
+        ]);
+
+        $free = Factory::create(new Product, [
+            'base_price' => 0,
+            'description_html' => '<p>This product tests a base price of zero</p>',
+            'name' => 'Free thing',
+            'slug' => 'free-thing',
+        ]);
+
+        $shirt = Factory::create(new Product, [
+            'base_price' => 19.99,
+            'description_html' => '<p>This product tests multiple inventories</p>',
+            'name' => 'Shirt',
+            'slug' => 'shirt',
+        ]);
 
         echo "  - Products\n";
     }
