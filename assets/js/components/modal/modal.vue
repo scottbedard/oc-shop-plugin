05f6508b46fef5fe1268cc3a147a6c10cd026511
scottbedard <scottbedard90@gmail.com>
2017-10-03 00:24:12 -0700

    modal animations, don't feel like fighting for existing css

--- assets/js/components/modal/modal.vue
+++ assets/js/components/modal/modal.vue
@@ -1,7 +1,36 @@
+<style lang="scss" scoped>@import 'core';
+    .modal-enter-active,
+    .modal-leave-active {
+        transition: all 150ms ease-in-out;
+
+        .modal-dialog {
+            transition: all 150ms ease-in-out;
+        }
+    }
+
+    .modal-enter,
+    .modal-leave-to {
+        background-color: rgba(0, 0, 0, 0);
+
+        .modal-dialog {
+            opacity: 0;
+            transform: scale(0.7);
+        }
+    }
+
+    .modal-enter-to,
+    .modal-leave {
+        .modal-dialog {
+            opacity: 1;
+            transform: scale(1);
+        }
+    }
+</style>
+
 <template>
-    <transition name="fade" mode="out-in">
+    <transition name="modal" mode="out-in">
         <div v-show="visible" class="popup-backdrop" @click="onBackdropClick">
-            <div class="control-popup modal fade" style="display: block" :class="inClass">
+            <div class="modal" style="display: block">
                 <div class="modal-dialog">
                     <div class="modal-content" @click.stop>
                         <slot />
@@ -14,16 +43,6 @@
 
 <script>
     export default {
-        data() {
-            return {
-                in: this.visible,
-            };
-        },
-        computed: {
-            inClass() {
-                return this.in ? 'in' : null;
-            },
-        },
         methods: {
             onBackdropClick() {
                 this.$emit('close');
@@ -33,11 +52,6 @@
             visible: {
                 required: true,
                 type: Boolean,
-            },
-        },
-        watch: {
-            visible(visible) {
-                this.$nextTick(() => this.$nextTick(() => this.in = visible));
             },
         },
     };
