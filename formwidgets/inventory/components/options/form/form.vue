71def35e8c076867f0d9aee77a02ce7b5150bc97
scottbedard <scottbedard90@gmail.com>
2017-10-18 23:00:20 -0700

    no need for this warning message anymore

--- formwidgets/inventory/components/options/form/form.vue
+++ formwidgets/inventory/components/options/form/form.vue
@@ -9,14 +9,6 @@
             <!-- body -->
             <v-modal-body>
 
-                <!-- delete message -->
-                <div v-if="isDeleted" class="callout fade in callout-warning no-subheader">
-                    <div class="header">
-                        <i class="icon-exclamation-triangle"></i>
-                        <p>{{ 'bedard.shop.options.form.delete_warning' | trans(lang) }}</p>
-                    </div>
-                </div>
-
                 <!-- name -->
                 <v-form-input
                     v-model="name"
@@ -62,7 +54,6 @@
         computed: {
             ...mapState('inventories', {
                 context: state => state.optionForm.context,
-                isDeleted: state => state.optionForm.data._delete,
                 isSaving: state => state.optionForm.isSaving,
                 isVisible: state => state.optionForm.isVisible,
                 lang: state => state.lang,
